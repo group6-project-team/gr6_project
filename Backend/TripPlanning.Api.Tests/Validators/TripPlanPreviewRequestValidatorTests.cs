86962a80801959215f755117de0e09d3e76c4e8d
MohammadSalameh10 <hamodesalameh747@gmail.com>
2026-09-20 15:08:26 +0300

    fix: support landmark public interest

---
+++ Backend/TripPlanning.Api.Tests/Validators/TripPlanPreviewRequestValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using TripPlanning.Api.DTOs.Requests;
+using TripPlanning.Api.Validators;
+
+namespace TripPlanning.Api.Tests.Validators
+{
+    public class TripPlanPreviewRequestValidatorTests
+    {
+        [Fact]
+        public async Task ValidateAsync_AcceptsLandmarkInterest()
+        {
+            var validator = new TripPlanPreviewRequestValidator();
+
+            var request = new TripPlanPreviewRequest
+            {
+                DestinationId = "istanbul",
+                Days = 3,
+                Interests = new List<string>
+                {
+                    "landmark"
+                }
+            };
+
+            var result = await validator.ValidateAsync(request);
+
+            Assert.True(result.IsValid);
+        }
+    }
+}
